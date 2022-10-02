a6de6cf503b1346f1bd2ddf74b791d8462d9083f
phuong.tran <phuong.tran@mesoneer.io>
2022-10-02 19:23:05 +0700

    update

--- Source/webpack4/6. Multiple Page Applications/extract-common5/src/kiwi.js
+++
@@ -1,8 +0,0 @@
-import Heading from "./components/heading/heading.js";
-import KiwiImage from "./components/kiwi-image/kiwi-image.js";
-import _ from "lodash";
-
-const heading = new Heading();
-heading.render(_.upperFirst("kiwi"));
-const kiwiImage = new KiwiImage();
-kiwiImage.render();
